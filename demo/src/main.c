b8c9acbfd680fcb9c214b2b19de272bfd8f0e658
pablomancera <pablo.mancera@hotmail.com>
2021-11-07 04:20:28 -0500

    modified demo

--- demo/src/main.c
+++ demo/src/main.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "list.h"
 
 int main() {
     list a = new_list();
     char *string1 = "esto es una prueba";
-    struct node *nodes_back[strlen(string1)];
+    struct node **nodes_back = malloc(strlen(string1) * sizeof(struct node*));
+
     for (int i = 0; i < strlen(string1); i++) {
         nodes_back[i] = list_push_back(&a, string1[i]);
     }
+
     char *string2 = (" ,eyO");
-    struct node *nodes_front[strlen(string2)];
+    struct node **nodes_front= malloc(strlen(string2) * sizeof(struct node*));
+
     for (int i = 0; i < strlen(string2); i++) {
         nodes_front[i] = list_push_front(&a, string2[i]);
     }
+
     list_print_all(&a);
+
     for (int i = 0; i < strlen(string1); i++) {
         list_add_before(&a, nodes_back[i], '.');
         list_add_after(&a, nodes_back[i], ',');
@@ -23,6 +29,10 @@ int main() {
         list_add_before(&a, nodes_front[i], ':');
         list_add_after(&a, nodes_front[i], ';');
     }
+
     list_to_string(&a);
+    
+    free(nodes_back);
+    free(nodes_front);
     return 0;
 }
